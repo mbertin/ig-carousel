cf6f2e8655d9081af595127ca1a06eedf4ba294d
Mathieu Bertin <mbertin@igraal.com>
2015-02-10 14:01:27 +0100

    Construct workspace

---
+++ src/directives/ig-carousel.js
@@ -0,0 +1,14 @@
+angular.application('ig-carousel').
+	directive('ig-carousel', ['', function() {
+
+		return {
+			restrict: "A",
+			scope: true,
+			compile: function() {
+
+			},
+			link: function(scope, element, attr) {
+
+			}
+		}
+	}]);
