745cf79691f9364b86ef5998adfce6c469a62481
Mathieu Bertin <mathieu.bertin56@gmail.com>
2015-02-16 13:32:41 +0100

    Remove useless vendors Add unit test (To be improved)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
-    rename = require('gulp-rename');
-
+    rename = require('gulp-rename'),
+    karma = require('karma').server;
 
 var destDir = "dist/";
 var destFileName = "ig-carousel.js";
@@ -15,10 +15,31 @@ gulp.task('build-dev', function () {
         .pipe(gulp.dest("./dist/"));
 });
 
+gulp.task('build-prod', function () {
+    return gulp.src('src/ig-carousel.js')
+        .pipe(browserify({
+            debug: true
+        }))
+        .pipe(rename('ig-carousel.js'))
+        .pipe(gulp.dest("./dist/"));
+});
+
 gulp.task('watch', function () {
     return gulp.watch(['src/**/*'], ['build-dev'], function(evt) {
         console.log('file has changed');
     });
 });
+
+/**
+ * Run test
+ */
+gulp.task('test', ['build-dev'], function (done) {
+
+    karma.start({
+        configFile: 'karma.conf.js',
+        singleRun: true
+    }, done);
+});
+
 
 gulp.task('default', ['watch']);
---
+++ test/initTest.js
@@ -0,0 +1,6 @@
+beforeEach(function () {
+    'use strict';
+
+    angular.module('test-ig-carousel', ['ig-carousel']);
+    module('test-ig-carousel');
+});
