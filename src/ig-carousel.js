735a5c315249d697d021acb6b0ba38096f92086d
Mathieu Bertin <mbertin@igraal.com>
2015-02-26 12:03:52 +0100

    Remove useless required

--- src/ig-carousel.js
+++ src/ig-carousel.js
@@ -2,10 +2,6 @@
  * Created by Mathieu Bertin (iGraal) on 10/02/2015.
  * Version 1.0.0
  */
-
-//Require use for browserify
-require('../bower_components/velocity/velocity.js');
-
 angular.module('ig-carousel', [])
 	.service('ig-service', require('./services/ig-service'))
 	.directive('igCarousel', require('./directives/ig-carousel'))
